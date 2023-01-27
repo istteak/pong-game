0e1358cb9a6387ef864197fc93ac6a9997283011
isteakua <121730770+isteakua@users.noreply.github.com>
2023-01-27 10:57:32 +0600

    Initial commit 1

--- main.py
+++ main.py
@@ -12,11 +12,14 @@ class PongBall(Widget):
         self.pos = Vector(*self.velocity) + self.pos
         
 class PongGame(Widget):
-    pass
+    def update(self, dt):
+        pass
 
 class PongApp(App):
     def build(self):
-        return PongGame()
+        game = PongGame()
+        Clock.schedule_interval(game.update, 1.0/60.0)
+        return game
 
 if __name__ == '__main__':
     PongApp().run()
