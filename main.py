26c8dbb37371a4270820f4b8b0c343926260507f
Isteak <123711367+istteak@users.noreply.github.com>
2023-01-27 11:41:59 +0600

    Initial commit 1

--- main.py
+++ main.py
@@ -29,10 +29,10 @@ class PongGame(Widget):
     ball = ObjectProperty(None)
     
     def on_touch_move(self, touch):
-    if touch.x < self.width/3:
-        self.player1.center_y = touch.y
-    if touch.x > self.width - self.width/3:
-        self.player2.center_y = touch.y
+        if touch.x < self.width/3:
+            self.player1.center_y = touch.y
+        if touch.x > self.width - self.width/3:
+            self.player2.center_y = touch.y
 
     def serve_ball(self):
         self.ball.center = self.center
