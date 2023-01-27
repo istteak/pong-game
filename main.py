7795daa1e7e7879a6ce5569fe62aa64732e98215
Isteak <123711367+istteak@users.noreply.github.com>
2023-01-27 11:03:14 +0600

    Initial commit 2

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import NumericProperty, ReferenceListProperty
 from kivy.vector import Vector
+from kivy.clock import Clock
 
 class PongBall(Widget):
     velocity_x = NumericProperty(0)
@@ -12,8 +13,17 @@ class PongBall(Widget):
         self.pos = Vector(*self.velocity) + self.pos
         
 class PongGame(Widget):
+    ball = ObjectProperty(None)
+
     def update(self, dt):
-        pass
+        self.ball.move()
+    
+        if (self.ball.y < 0) or (self.ball.top > self.height):
+            self.ball.velocity_y *= -1
+
+        # bounce off left and right
+        if (self.ball.x < 0) or (self.ball.right > self.width):
+            self.ball.velocity_x *= -1
 
 class PongApp(App):
     def build(self):
